39b635be69770fae20115ae85a1c76c5e6944f84
NicoNandez <nico220916@gmail.com>
2025-12-15 19:27:49 -0500

    Add registration route to authRoutes

--- helpdesk-backend/routes/authRoutes.js
+++ helpdesk-backend/routes/authRoutes.js
@@ -1,1 +1,8 @@
-const express=require("express"); const router=express.Router(); const c=require("../controllers/authController"); router.post("/login",c.login); module.exports=router;
+const express = require("express");
+const router = express.Router();
+const c = require("../controllers/authController");
+
+router.post("/login", c.login);
+router.post("/register", c.register);
+
+module.exports = router;
